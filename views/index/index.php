0df22795bd77c679f26be4bdd4891874d189878a
vietpn <vietpnk53@gmail.com>
2016-07-29 20:46:15 +0700

    update

--- views/index/index.php
+++ views/index/index.php
@@ -1,7 +1,7 @@
 <?php foreach ($this->posts as $post): ?>
     <div class="row">
         <h2>
-            <a href="<?php echo URL; ?>post/read/<?php echo $post['id']; ?>"><?php echo $post['title']; ?></a>
+            <a href="<?php echo URL; ?>post/read/<?php echo $post['id']; ?>"><?php echo ucfirst($post['title']); ?></a>
         </h2>
 
         <p><?php echo $post['date_created']; ?></p>
--- views/post/read.php
+++ views/post/read.php
@@ -1,5 +1,5 @@
 <?php if (!empty($this->post)): ?>
-    <h3><?php echo $this->post['title']; ?></h3>
+    <h1><?php echo ucfirst($this->post['title']); ?></h1>
     <?php if (Session::get('loggedIn') == true): ?>
         <label><a href="<?php echo URL . 'post/delete/' . $this->post['id']; ?>">[delete]</a></label>
         <label><a href="<?php echo URL . 'post/update/' . $this->post['id']; ?>">[update]</a></label>
@@ -17,7 +17,7 @@
 <?php endif; ?>
 
 
-    <h3>Leave comments!</h3>
+    <h3>Leave your comments!</h3>
 <?php if (!empty($this->post)): ?>
     <?php if (!empty($this->errors)): ?>
         <ul class="error">
@@ -30,7 +30,7 @@
         <?php if (Session::get('loggedIn') == true && !empty($this->user)) { ?>
             <input type="hidden" name="username" value="<?php echo $this->user['username']; ?>">
         <?php } else { ?>
-            <label>User name</label><input type="text" name="username"/><br/>
+            <label>Name</label><input type="text" name="username"/><br/>
         <?php } ?>
         <label>Comment</label><textarea cols="30" rows="5" name="content"></textarea><br/>
         <label></label><input type="submit"/>
