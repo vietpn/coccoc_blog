d24a22084c29ba6981f9fac9a1fc943d506331ff
vietpn <vietpnk53@gmail.com>
2018-07-30 15:46:46 +0700

    test

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by IntelliJ IDEA.
  * User: vietpn
@@ -8,21 +9,23 @@
 
 
 // load library!
-require 'libs/Bootstrap.php';
-require 'libs/Controller.php';
-require 'libs/Model.php';
-require 'libs/View.php';
-require 'libs/Database.php';
-require 'libs/Session.php';
-require 'libs/DbInterface.php';
-require 'libs/DbModel.php';
-require 'libs/FormInterface.php';
-require 'libs/FormModel.php';
+// require 'libs/Bootstrap.php';
+// require 'libs/Controller.php';
+// require 'libs/Model.php';
+// require 'libs/View.php';
+// require 'libs/Database.php';
+// require 'libs/Session.php';
+// require 'libs/DbInterface.php';
+// require 'libs/DbModel.php';
+// require 'libs/FormInterface.php';
+// require 'libs/FormModel.php';
 
-require 'config/paths.php';
-require 'config/database.php';
+// require 'config/paths.php';
+// require 'config/database.php';
 
-require 'util/Auth.php';
+// require 'util/Auth.php';
 
-$app = new Bootstrap();
+// $app = new Bootstrap();
+
+echo 'test';
 ?>
